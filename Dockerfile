a9b229fb6593e0b0c1f7b36d400563bec3a884aa
ackleymi <ackleymi@gmail.com>
2019-11-19 00:02:38 -0500

    Adds netlify deploy.

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM piquette/caddy:latest
-LABEL maintainer="Michael Ackley <ackleymi@gmail.com>"
-COPY public /var/www/html
-COPY Caddyfile /var/www/html
--- travis-docker-deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD";
-docker push piquette/web:latest
-docker push piquette/web:$SITE_VERSION
